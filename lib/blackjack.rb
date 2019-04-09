02a82206bd10f31b2c830254eb88ca246d16f00a
Paula Tulis <tulis.paula@gmail.com>
2019-04-09 20:44:58 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -6,8 +6,8 @@ def deal_card
   rand(1..11)
 end
 
-def display_card_total(current_card_total)
-  puts "Your cards add up to #{current_card_total}"
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
   # code #display_card_total here
 end
 
